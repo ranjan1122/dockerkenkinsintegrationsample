f5a35eddfbec6fb029c40695945185848865923c
Rashmi <rashmi.rrp@gmail.com>
2023-04-30 00:43:29 +0530

    Just adding two files from outside

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Docker Jenkin integration!!!");
           System.out.println("sending this from different branch and location");
+           System.out.println("Adding this for second time!!!");
 
     }
 }
